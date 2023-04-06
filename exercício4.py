65d07b9a074b20026a646f7628827afec2c80ff7
33 <tommaldonado444@gmail.com>
2023-04-06 16:23:59 -0300

    finalizado o ex 4

--- exercício4.py
+++ exercício4.py
@@ -1,4 +1,4 @@
 exercici04 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-for i in exercici04:
-    if i not in "a,e,i,o,u":
-        print(i)
+for u in exercici04:
+    if u not in "a,e,i,o,u":
+        print(u)
